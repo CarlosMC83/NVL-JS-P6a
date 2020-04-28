5cb50ccc47926f560b52df179f72f70a4fc913ae
Carlos Martínez <carlosmartinezlinkedin@gmail.com>
2020-04-28 21:44:31 +0200

    Mensaje de referencia

---
+++ main.js
@@ -0,0 +1,9 @@
+var nombre y apellidos = "Carlos Martinez Catalan"
+
+var edad = 36;
+
+var gravedad = 6.674;
+
+var dias de la semana = "lunes, martes, miercoles, jueves, viernes"
+
+var numeros impares = "1, 3, 5"
